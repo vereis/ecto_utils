2cecdc7ed73d9e0081b359ec071e97b7e0b1a11e
Chris Bailey <me@cbailey.co.uk>
2022-03-29 22:12:31 +0000

    Add `list_preloads/1`

--- lib/ecto_utils/repo.ex
+++ lib/ecto_utils/repo.ex
@@ -15,4 +15,54 @@ defmodule EctoUtils.Repo do
   def is_schema(%module{} = _struct) do
     is_schema(module)
   end
+
+  @doc """
+  Given a struct derived from a module that `use`-es `Ecto.Schema`, returns the list
+  of currently preloaded associations for said schema.
+
+  This can be useful when used in conjunction with `Repo.reload/2` to refetch a given
+  struct from the database while also reloading all preloads:
+
+  ```elixir
+  user = %MyApp.User{orgs: [...]}
+  preloads = MyApp.Repo.list_preloads(user)
+
+  user
+  |> MyApp.Repo.reload()
+  |> MyApp.Repo.preload(preloads)
+  ```
+  """
+  @spec list_preloads(struct()) :: [atom()]
+  def list_preloads(%module{} = record) do
+    unless is_schema(module) do
+      raise ArgumentError,
+        message: "Expected an Ecto schema struct, given: `%#{inspect(module)}{}`"
+    end
+
+    do_list_preloads(record)
+  end
+
+  defp do_list_preloads(%schema{} = record) when schema != Ecto.Association.NotLoaded do
+    associations = schema.__schema__(:associations)
+
+    record
+    |> Map.from_struct()
+    |> Enum.flat_map(fn {field, value} ->
+      if not match?(%{__struct__: Ecto.Association.NotLoaded}, value) and field in associations do
+        value
+        |> do_list_preloads()
+        |> case do
+          [] ->
+            [field]
+
+          nested_fields ->
+            [{field, nested_fields}]
+        end
+      else
+        []
+      end
+    end)
+  end
+
+  defp do_list_preloads(_value), do: []
 end
--- test/ecto_utils/repo_test.exs
+++ test/ecto_utils/repo_test.exs
@@ -11,6 +11,46 @@ defmodule EctoUtils.RepoTest do
     end
   end
 
+  defmodule Jar do
+    use Ecto.Schema
+
+    schema "jar" do
+      has_many(:cucumbers, EctoUtils.RepoTest.Cucumber)
+      belongs_to(:parent_jar, __MODULE__)
+    end
+  end
+
+  describe "list_preloads/1" do
+    test "returns empty list given schema with no associations" do
+      assert [] = Repo.list_preloads(%Cucumber{})
+    end
+
+    test "returns empty list given schema with associations, but nothing preloaded" do
+      assert [] = Repo.list_preloads(%Jar{})
+    end
+
+    test "returns list of potentially nested preloaded fields on given schema" do
+      cucumber = %Cucumber{}
+
+      jar = %Jar{
+        parent_jar: %Jar{
+          cucumbers: [cucumber],
+          parent_jar: %Jar{
+            cucumbers: [cucumber, cucumber, cucumber],
+            parent_jar: %Jar{cucumbers: []}
+          }
+        }
+      }
+
+      assert [parent_jar: [:cucumbers, parent_jar: [:cucumbers, parent_jar: [:cucumbers]]]] =
+               Repo.list_preloads(jar)
+    end
+
+    test "raises when given anything that isn't a struct from a module that `use`-es `Ecto.Schema`" do
+      assert_raise ArgumentError, fn -> Repo.list_preloads(Date.utc_today()) end
+    end
+  end
+
   describe "is_schema/1" do
     test "returns true if given module that `use`-es `Ecto.Schema`" do
       assert Repo.is_schema(Cucumber)
