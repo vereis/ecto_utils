f50ea168a5a51485a1a41d497ec9261824b94ac9
Chris Bailey <me@cbailey.co.uk>
2022-03-29 21:21:36 +0000

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,54 @@
+defmodule EctoUtils.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      aliases: aliases(),
+      app: :ecto_utils,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      elixirc_options: [warnings_as_errors: true],
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # Lint dependencies
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+
+      # Test dependencies
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+
+      # Misc dependencies
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [lint: ["format --check-formatted --dry-run", "credo --strict", "dialyzer"]]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/vereis/ecto_utils"
+      }
+    ]
+  end
+end
