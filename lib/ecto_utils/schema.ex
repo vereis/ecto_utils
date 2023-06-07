58e9cf8775854384e8a787b4fea94e0efde4c14a
Chris Bailey <me@cbailey.co.uk>
2023-06-07 14:36:34 +0100

    Refine and remove complex pluralization in `preload_put_assoc/4`

--- lib/ecto_utils/schema.ex
+++ lib/ecto_utils/schema.ex
@@ -45,36 +45,95 @@ defmodule EctoUtils.Schema do
 
     [
       quote do
+        import unquote(__MODULE__)
+
         @doc """
         Helper function for handling the boilerplate of preloading associations before setting them
         as an association, particularly useful for many-to-many or has-many relations.
         """
         @spec preload_put_assoc(Ecto.Changeset.t(), attrs :: map(), field :: atom()) ::
                 Ecto.Changeset.t()
-        def preload_put_assoc(changeset, attrs, field, filters \\ [])
+        def preload_put_assoc(changeset, attrs, field, attr_key \\ nil, filters \\ [])
 
-        def preload_put_assoc(%Ecto.Changeset{} = changeset, attrs, field, filters) do
-          unquote(schema).preload_put_assoc(unquote(repo), changeset, attrs, field, filters)
+        def preload_put_assoc(%Ecto.Changeset{} = changeset, attrs, field, attr_key, filters) do
+          unquote(schema).preload_put_assoc(
+            unquote(repo),
+            changeset,
+            attrs,
+            field,
+            attr_key || field,
+            filters
+          )
         end
+
+        @doc """
+        Helper function for returning a list of valid fields for this schema. Useful for casting changeset
+        attrs.
+        """
+        @spec fields :: [atom()]
+        def fields, do: __schema__(:fields)
+
+        @doc """
+        Helper function for returning a list of valid associations (`belongs_to`, `has_one`, `has_many`, or
+        `many_to_many` fields) for this schema.
+        """
+        @spec associations :: [atom()]
+        def associations, do: __schema__(:associations)
+
+        @doc """
+        Helper function for looking up metadata about a specific association for this schema.
+        """
+        @spec association(atom()) :: Ecto.Association.t()
+        def association(field), do: __schema__(:association, field)
       end
       | delegates
     ]
+  end
+
+  defmacro join_schema(table, {from_id, to_id}) when is_binary(table) do
+    module = Module.concat([EctoUtils.Dyn.JoinSchemas, __CALLER__.module, Macro.camelize(table)])
+
+    module_declaration =
+      quote bind_quoted: binding() do
+        defmodule module do
+          use Ecto.Schema
+
+          @primary_key false
+          schema table do
+            field(from_id, :id)
+            field(to_id, :id)
+            timestamps()
+          end
+        end
+      end
+
+    quote bind_quoted: binding(), location: :keep do
+      {:module, _name, _bytecode, _env} = module_declaration
+      module
+    end
   end
 
   @doc """
   Helper function for handling the boilerplate of preloading associations before setting them
   as an association, particularly useful for many-to-many or has-many relations.
   """
-  @spec preload_put_assoc(repo :: module(), Ecto.Changeset.t(), attrs :: map(), field :: atom()) ::
+  @spec preload_put_assoc(
+          repo :: module(),
+          Ecto.Changeset.t(),
+          attrs :: map(),
+          field :: atom(),
+          attr_key :: atom(),
+          filters :: Keyword.t()
+        ) ::
           Ecto.Changeset.t()
-  def preload_put_assoc(repo, changeset, attrs, field, filters \\ [])
+  def preload_put_assoc(repo, changeset, attrs, field, attr_key, filters \\ [])
 
-  def preload_put_assoc(repo, %Ecto.Changeset{} = changeset, attrs, field, filters) do
-    case parse_field(attrs, field) do
-      {_field, nil} ->
+  def preload_put_assoc(repo, %Ecto.Changeset{} = changeset, attrs, field, attr_key, filters) do
+    case Map.get(attrs, attr_key) do
+      nil ->
         changeset
 
-      {field, item} ->
+      item ->
         items = handle_preload_put_assoc_items(repo, changeset, field, item, filters)
 
         changeset
@@ -82,21 +141,6 @@ defmodule EctoUtils.Schema do
         |> repo.preload(field)
         |> then(fn data -> %Ecto.Changeset{changeset | data: data} end)
         |> Ecto.Changeset.put_assoc(field, items)
-    end
-  end
-
-  defp parse_field(attrs, field) do
-    field_string = to_string(field)
-
-    if String.ends_with?(field_string, "_ids") do
-      item =
-        field_string
-        |> pluralize()
-        |> String.to_existing_atom()
-
-      {item, attrs[field]}
-    else
-      {field, attrs[field]}
     end
   end
 
@@ -125,27 +169,6 @@ defmodule EctoUtils.Schema do
 
       true ->
         raise "Function `preload_put_assoc/3` expects items to either be a list of IDs, or a list of structs, got: #{inspect(items)}"
-    end
-  end
-
-  # TODO: make this configurable
-  defp pluralize(word) do
-    word =
-      if String.ends_with?(word, "_ids") do
-        String.trim_trailing(word, "_ids")
-      else
-        word
-      end
-
-    cond do
-      String.ends_with?(word, "ey") ->
-        String.replace_suffix(word, "ey", "ies")
-
-      String.ends_with?(word, "y") ->
-        String.replace_suffix(word, "y", "ies")
-
-      true ->
-        word <> "s"
     end
   end
 
